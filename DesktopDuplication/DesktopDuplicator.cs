b4973b652fb1e1eaa84c07be9224fa774f85e1ac
Jason Pang <jasonpang2011@gmail.com>
2015-05-25 16:39:20 -0700

    Implemented DesktopDuplicator ctor()

--- DesktopDuplication/DesktopDuplicator.cs
+++ DesktopDuplication/DesktopDuplicator.cs
@@ -1,8 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.IO;
+
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+using Rectangle = SharpDX.Rectangle;
+using System.Drawing;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace DesktopDuplication
 {
@@ -15,6 +23,11 @@ namespace DesktopDuplication
         /// Gets the last updated frame.
         /// </summary>
         public DesktopFrame Frame { get; private set; }
+
+        private Device mDevice;
+        private Texture2DDescription mTextureDesc;
+        private OutputDescription mOutputDesc;
+        private OutputDuplication mDeskDupl;
 
         /// <summary>
         /// Duplicates the output of the specified monitor.
@@ -30,7 +43,53 @@ namespace DesktopDuplication
         /// <param name="whichOutputDevice">The output device to duplicate (i.e. monitor). Begins with zero, which seems to correspond to the primary monitor.</param>
         public DesktopDuplicator(int whichGraphicsCardAdapter, int whichOutputDevice)
         {
-            throw new NotImplementedException();
+            Frame = new DesktopFrame();
+            Adapter1 adapter = null;
+            try
+            {
+                adapter = new Factory1().GetAdapter1(whichGraphicsCardAdapter);
+            }
+            catch (SharpDXException)
+            {
+                throw new DesktopDuplicationException("Could not find the specified graphics card adapter.");
+            }
+            this.mDevice = new Device(adapter);
+            Output output = null;
+            try
+            {
+                output = adapter.GetOutput(whichOutputDevice);
+            }
+            catch (SharpDXException)
+            {
+                throw new DesktopDuplicationException("Could not find the specified output device.");
+            }
+            var output1 = output.QueryInterface<Output1>();
+            this.mOutputDesc = output.Description;
+            this.mTextureDesc = new Texture2DDescription()
+            {
+                CpuAccessFlags = CpuAccessFlags.Read,
+                BindFlags = BindFlags.None,
+                Format = Format.B8G8R8A8_UNorm,
+                Width = this.mOutputDesc.DesktopBounds.Width,
+                Height = this.mOutputDesc.DesktopBounds.Height,
+                OptionFlags = ResourceOptionFlags.None,
+                MipLevels = 1,
+                ArraySize = 1,
+                SampleDescription = { Count = 1, Quality = 0 },
+                Usage = ResourceUsage.Staging
+            };
+
+            try
+            {
+                this.mDeskDupl = output1.DuplicateOutput(mDevice);
+            }
+            catch (SharpDXException ex)
+            {
+                if (ex.ResultCode.Code == SharpDX.DXGI.ResultCode.NotCurrentlyAvailable.Result.Code)
+                {
+                    throw new DesktopDuplicationException("There is already the maximum number of applications using the Desktop Duplication API running, please close one of the applications and try again.");
+                }
+            }
         }
 
         /// <summary>
