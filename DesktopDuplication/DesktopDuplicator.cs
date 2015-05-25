25adfd3a0aa06e7447484c3e54855000665083b6
Jason Pang <jasonpang2011@gmail.com>
2015-05-25 15:49:07 -0700

    Initial project structure and method stubs

---
+++ DesktopDuplication/DesktopDuplicator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopDuplication
+{
+    /// <summary>
+    /// Provides access to frame-by-frame updates of a particular desktop (i.e. one monitor), with image and cursor information.
+    /// </summary>
+    public class DesktopDuplicator
+    {
+        /// <summary>
+        /// Gets the last updated frame.
+        /// </summary>
+        public DesktopFrame Frame { get; private set; }
+
+        /// <summary>
+        /// Duplicates the output of the specified monitor.
+        /// </summary>
+        /// <param name="whichMonitor">The output device to duplicate (i.e. monitor). Begins with zero, which seems to correspond to the primary monitor.</param>
+        public DesktopDuplicator(int whichMonitor)
+            : this(0, whichMonitor) { }
+
+        /// <summary>
+        /// Duplicates the output of the specified monitor on the specified graphics adapter.
+        /// </summary>
+        /// <param name="whichGraphicsCardAdapter">The adapter which contains the desired outputs.</param>
+        /// <param name="whichOutputDevice">The output device to duplicate (i.e. monitor). Begins with zero, which seems to correspond to the primary monitor.</param>
+        public DesktopDuplicator(int whichGraphicsCardAdapter, int whichOutputDevice)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retrieves and stores the latest desktop image and metadata.
+        /// </summary>
+        public void UpdateFrame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
