4b426948f6137e1bad0995b7cd68a53ee9b1c6c6
Jason Pang <jasonpang2011@gmail.com>
2015-05-25 17:28:07 -0700

    Added basic demo app.

---
+++ DesktopDuplication.Demo/FormDemo.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace DesktopDuplication.Demo
+{
+    partial class FormDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 431);
+            this.DoubleBuffered = true;
+            this.Name = "FormDemo";
+            this.Text = "Desktop Duplication API Demo";
+            this.Shown += new System.EventHandler(this.FormDemo_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ DesktopDuplication.Demo/FormDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopDuplication.Demo
+{
+    public partial class FormDemo : Form
+    {
+        private DesktopDuplicator desktopDuplicator;
+
+        public FormDemo()
+        {
+            InitializeComponent();
+
+            try
+            {
+                desktopDuplicator = new DesktopDuplicator(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void FormDemo_Shown(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                Application.DoEvents();
+                desktopDuplicator.UpdateFrame();
+                this.BackgroundImage = desktopDuplicator.Frame.DesktopImage;
+            }
+        }
+    }
+}
